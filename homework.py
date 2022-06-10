8c3e4f9ff3d054b83bdb2ff74d37d12729eaf856
Heinrich Virt <heinrich.ego.1997@gmail.com>
2022-06-10 22:09:26 +0300

    -

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
-from typing import Dict, Type, ClassVar
-from dataclasses import dataclass
+from typing import Dict, Type
+from dataclasses import dataclass, asdict
 
 
 # Очень надеюсь, что теперь верно.
@@ -15,21 +15,16 @@ class InfoMessage:
     speed: float
     calories: float
 
-    PATTERN: ClassVar[str] = (
-        'Тип тренировки: {}; '
-        'Длительность: {:.3f} ч.; '
-        'Дистанция: {:.3f} км; '
-        'Ср. скорость: {:.3f} км/ч; '
-        'Потрачено ккал: {:.3f}.'
+    PATTERN = (
+        'Тип тренировки: {training_type}; '
+        'Длительность: {duration:.3f} ч.; '
+        'Дистанция: {distance:.3f} км; '
+        'Ср. скорость: {speed:.3f} км/ч; '
+        'Потрачено ккал: {calories:.3f}.'
     )
 
     def get_message(self) -> str:
-        text = self.PATTERN.format(
-            self.training_type, self.duration,
-            self.distance, self.speed,
-            self.calories
-        )
-        return text
+        return self.PATTERN.format(**asdict(self))
 
 
 class Training:
